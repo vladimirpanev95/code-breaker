d82049e116f6d2b96cb9f82ac0fa677cb36aa43a
vladimirpanev95 <vladimirpanev95@gmail.com>
2024-12-21 20:35:48 +0200

    Create functions for create SimpleStorage contract and two functions for it

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -5,10 +5,22 @@ import {SimpleStorage} from "contracts/SimpleStorage.sol";
 
 contract StorageFactory{
 
-   SimpleStorage public simpleStorage;
+   SimpleStorage[] public listOfSimpleStorageContract;
 
    function createSimpleStorageContract() public {
-       simpleStorage = new SimpleStorage();
+       SimpleStorage newSimpleStorageContract = new SimpleStorage();
+       listOfSimpleStorageContract.push(newSimpleStorageContract);
+   }
+
+   function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+    // Address
+    //ABI - Application Binary Interface
+    SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+    mySimpleStorage.store(_newSimpleStorageNumber);
+   }
+
+   function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+    return  listOfSimpleStorageContract[_simpleStorageIndex].retrieve();
    }
 
 }
